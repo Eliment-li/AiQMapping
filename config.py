ce1a3a3f37b8c328ebe082c61d27042a5fdf9210
Eliment-li <felix.litian@foxmail.com>
2024-10-15 11:27:05 +0800

    add utils

---
+++ config.py
@@ -0,0 +1,47 @@
+import os
+
+from munch import Munch
+import torch
+import yaml
+
+from utils.file.file_util import get_root_dir
+
+def get_args():
+    config = None
+    rootdir = get_root_dir()
+    with open(rootdir+os.path.sep+'config.yml', 'r') as file:
+        config = yaml.safe_load(file)
+        config['device'] = "cuda" if torch.cuda.is_available() else "cpu"
+        config = Munch(config)
+    return config
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class ConfigSingleton(metaclass=Singleton):
+    def __init__(self):
+        self.config = None
+        self.load_config()
+        self.flag = 0
+
+    def load_config(self):
+        rootdir = get_root_dir()
+        with open(rootdir+os.path.sep+'config.yml', 'r') as file:
+            config = yaml.safe_load(file)
+            config['device'] = "cuda" if torch.cuda.is_available() else "cpu"
+            self.config = Munch(config)
+
+    def get_config(self):
+        self.flag += 1
+        return self.config
+
+if __name__ == '__main__':
+    args = ConfigSingleton().get_config()
+    print(ConfigSingleton().get_config().qasm)
+    ConfigSingleton().get_config().qasm = 'test'
+    print(ConfigSingleton().get_config().qasm)
--- utils/file/file_util.py
+++ utils/file/file_util.py
@@ -1,7 +1,5 @@
 import os
 
-import qiskit
-from qiskit import QuantumCircuit
 import os
 import shutil
 import zipfile
@@ -72,6 +70,55 @@ def copy(path1,path2,file_name):
     shutil.copy(path1, os.path.join(path2, file_name))
     print(f"成功将文件夹 {path1} 下的文件打包为 {file_name} 并复制到 {path2}")
 
+
+
+
+def find_files_with_suffix(directory, suffix=None):
+
+    # 存储符合条件的文件名
+    matching_files = []
+
+    # 使用 os.walk 遍历目录及其子目录
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            # 检查文件是否以指定后缀结尾
+            if suffix is None or file.endswith(suffix):
+                # 将符合条件的文件路径加入列表
+                matching_files.append(os.path.join(root, file))
+
+    return matching_files
+
+def convert(data):
+    import json
+    data = json.loads(data)
+    content = ''
+    for i, code in enumerate(data['exp_codes']):
+        print(f"Processed exp_code {i + 1}:")
+        for line in code.split('\n'):
+            print(line)
+            content += line
+            content += '\n'
+    return content
+
+import re
+
+
+def replace_multiple_words(text, replacements):
+    # 构建正则表达式模式，匹配所有需要替换的独立单词
+    pattern = r'\b(' + '|'.join(re.escape(word) for word in replacements.keys()) + r')\b'
+
+    # 定义替换函数
+    def replacer(match):
+        # 返回匹配的单词在字典中的替换值
+        return replacements[match.group(0)]
+
+    # 使用 re.sub 进行替换
+    replaced_text = re.sub(pattern, replacer, text)
+    return replaced_text
+
+
+
+
 if __name__ == '__main__':
     # 调用示例
     # path1 = "d:/test"
@@ -79,9 +126,28 @@ if __name__ == '__main__':
     # zip_path,zip_file_name = compress_folder(path1)
     # copy(zip_path,path2,zip_file_name)
 
-    content = read_all('data/test_set/xeb/xeb3/XEB_3_qubits_8_cycles_circuit.txt')
-    print(content)
+    # content = read_all('data/test_set/xeb/xeb3/XEB_3_qubits_8_cycles_circuit.txt')
+    # print(content)
 
+    #
+    # dir_path = r'D:\workspace\电信大赛\3_66_ghz_circuit\3_66_ghz_circuit\3_66_ghz_circuit'
+    # files = find_files_with_suffix(dir_path)
+    #
+    # for file in files:
+    #     content = read_all(file)
+    #     new_content = convert(content)
+    #     write(file.replace('.json','.txt'),new_content)
+
+    # 示例用法
+    text = read_all(r'D:\workspace\电信大赛\3_66_ghz_circuit\3_66_ghz_circuit\3_66_ghz_circuit\3qubit_ghz.txt')
+
+    replacements = {
+        "Q0": "Q21",
+        "Q1": "Q26",
+        "Q2": "Q33"
+    }
+    result = replace_multiple_words(text,replacements)
+    print(result)
 
 
 
