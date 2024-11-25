815aa5526da70f7cdb8396811970a92e130c6ddf
Eliment-li <felix.litian@foxmail.com>
2024-11-25 11:14:53 +0800

    merge train.py to evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from copy import deepcopy
+from pathlib import Path
+
 import numpy as np
 from ray.rllib.algorithms import Algorithm
 import gymnasium as gym
@@ -7,9 +9,9 @@ from sympy import pprint
 
 from config import ConfigSingleton
 from env.env_helper import register_custom_env
+from utils.file.file_util import get_root_dir
 from  utils.visualize.trace import show_trace, show_result
-from utils.visualize.train import show_train_metric
-
+import matplotlib.pyplot as plt
 args = ConfigSingleton().get_config()
 max_step = 100
 r_arr = []
@@ -67,10 +69,11 @@ def evaluate_policy(results):
     # if args.show_trace:
     #     show_trace(trace.transpose())
     show_result(trace[-1])
-    show_train_metric([r_arr,dist_arr])
+    plot_evaluate([r_arr,dist_arr])
 
     #use attention
 def evaluate_policyv2(results):
+    args = ConfigSingleton().get_config()
     if  not isinstance(results, str):
         checkpoint = results.get_best_result(metric='env_runners/episode_reward_mean', mode='max').checkpoint
         checkpoint = checkpoint.to_directory()
@@ -150,7 +153,69 @@ def evaluate_policyv2(results):
     # if args.show_trace:
     #     show_trace(trace.transpose())
     show_result(trace[-1])
-    show_train_metric([r_arr, nn_arr, dist_arr])
+    plot_evaluate([r_arr, nn_arr, dist_arr])
+
+    from utils.file.file_util import get_root_dir
+    args = ConfigSingleton().get_config()
+
+# data[0]:  data[1]:
+def plot_evaluate(data, save=True, max_length=20):
+    '''
+    # Example usage:
+    data = [
+        [10, 20, 30, 40, 50],
+        [0, 342, 200, 500, 600],
+        [3, 4, 5, 6, 7]
+    ]
+    label = ['Metric 1', 'Metric 2', 'Metric 3']
+
+    plot_evaluate(data, label)
+    '''
+
+    # 创建一个图形和一个坐标轴
+    fig, ax1 = plt.subplots()
+
+    # Generate a colormap
+    num_lines = len(data)
+    colors = plt.cm.viridis(np.linspace(0, 1, num_lines))
+
+    x1 = np.arange(len(data[0][:max_length]))
+    y1 = data[0][:max_length]
+
+    x2 = np.arange(len(data[1][:max_length]))
+    y2 = data[1][:max_length]
+
+    ax1.plot(x1, y1, label='reward', color='#5370c4', marker='o')
+    ax1.set_xlabel('step')
+    ax1.set_ylabel('reward', color='#5370c4')
+    for x, y in zip(x1, y1):
+        # Annotate each point with its value
+        ax1.annotate(f'{y:.2f}', (x, y), textcoords="offset points", xytext=(0, 10), ha='center')
+
+        # Draw a vertical line from each point to the x-axis
+        ax1.axvline(x=x, ymin=0, ymax=(y - plt.ylim()[0]) / (plt.ylim()[1] - plt.ylim()[0]), color=colors[0],
+                    linestyle='--', linewidth=0.5)
+
+    # 创建第二个坐标轴，共享x轴
+    ax2 = ax1.twinx()
+    ax2.plot(x2, y2, label='distance', color='#f16569', marker='v')
+    ax2.set_ylabel('distance', color='#f16569')
+    for x, y in zip(x2, y2):
+        # Annotate each point with its value
+        ax2.annotate(f'{y:.0f}', (x, y), textcoords="offset points", xytext=(0, 10), ha='center')
+
+    lines_1, labels_1 = ax1.get_legend_handles_labels()
+    lines_2, labels_2 = ax2.get_legend_handles_labels()
+    ax1.legend(lines_1 + lines_2, labels_1 + labels_2, loc='upper left')
+    # Add labels and legend
+    plt.title('Evaluate Result')
+
+    if save:
+        path = Path(get_root_dir()) / 'data' / 'result' / (args.time_id + '.png')
+        plt.savefig(path)
+    # Show the plot
+    # plt.show()
+
 
 if __name__ == '__main__':
     register_custom_env(args.env_version)
